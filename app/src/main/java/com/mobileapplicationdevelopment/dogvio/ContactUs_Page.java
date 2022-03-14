2d2eb97c4ef9954d5632c61fe4ca00047b01d1f9
Pasindu Prabhashitha <it20610166@my.sliit.lk>
2022-03-14 20:54:21 +0530

    Rename application name

--- app/src/main/java/com/mobileapplicationdevelopment/dogvio/ContactUs_Page.java
+++ app/src/main/java/com/mobileapplicationdevelopment/dogvio/ContactUs_Page.java
@@ -1,4 +1,4 @@
-package com.example.dogapp;
+package com.mobileapplicationdevelopment.dogvio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/mobileapplicationdevelopment/dogvio/Old_Customer.java
+++ app/src/main/java/com/mobileapplicationdevelopment/dogvio/Old_Customer.java
@@ -1,4 +1,4 @@
-package com.example.dogapp;
+package com.mobileapplicationdevelopment.dogvio;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,28 +8,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Monthly_Memberships extends AppCompatActivity {
+public class Old_Customer extends AppCompatActivity {
 
-    Button button3;
+ Button button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monthly_memberships);
+        setContentView(R.layout.activity_old_customer);
 
-        ActionBar actionBar1 = getSupportActionBar();
-        actionBar1.setTitle("Monthly Membership");
-        actionBar1.setDisplayShowHomeEnabled(true);
-        actionBar1.setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar3 = getSupportActionBar();
+        actionBar3.setTitle("Day Care Service");
+        actionBar3.setDisplayShowHomeEnabled(true);
+        actionBar3.setDisplayHomeAsUpEnabled(true);
 
 
-        button3 = findViewById(R.id.button5);
+        button3 = findViewById(R.id.button);
 
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent3 = new Intent(getApplicationContext(), Booking_DayCare.class);
+                Intent intent3 = new Intent(getApplicationContext(), Monthly_Memberships.class);
                 startActivity(intent3);
             }
         });
+
     }
 }
