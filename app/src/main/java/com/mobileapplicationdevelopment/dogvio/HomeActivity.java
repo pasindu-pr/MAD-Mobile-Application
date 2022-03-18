5fd6dc1540fa1520d1b6d7352518a7ddb5bdc50e
Pasindu Prabhashitha <it20610166@my.sliit.lk>
2022-03-18 19:48:04 +0530

    Add intents for navigation

--- app/src/main/java/com/mobileapplicationdevelopment/dogvio/HomeActivity.java
+++ app/src/main/java/com/mobileapplicationdevelopment/dogvio/HomeActivity.java
@@ -2,15 +2,24 @@ package com.mobileapplicationdevelopment.dogvio;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 import com.mobileapplicationdevelopment.dogvio.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private AppCompatButton getStartedButton;
+
+    private void onGetStartedButtonClick () {
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,15 @@ public class HomeActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_home);
+
+        getStartedButton = (AppCompatButton) findViewById(R.id.getStartedButton);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onGetStartedButtonClick();
+            }
+        });
 
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.purple_500));
--- app/src/main/java/com/mobileapplicationdevelopment/dogvio/LoginPage.java
+++ app/src/main/java/com/mobileapplicationdevelopment/dogvio/LoginPage.java
@@ -2,10 +2,28 @@ package com.mobileapplicationdevelopment.dogvio;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
+
+    private AppCompatButton loginButton;
+    private TextView notRegisteredText;
+
+    private void onLoginButtonClick () {
+        Intent intent = new Intent(this, NavigationPage.class);
+        startActivity(intent);
+    }
+
+    private void notRegisteredTextClick () {
+        Intent intent = new Intent(this, RegistrationPage.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +31,23 @@ public class LoginPage extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_login_page);
+
+        loginButton = (AppCompatButton) findViewById(R.id.loginButton);
+        notRegisteredText = (TextView) findViewById(R.id.notRegisteredText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLoginButtonClick();
+            }
+        });
+
+        notRegisteredText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notRegisteredTextClick();
+            }
+        });
+
     }
 }
