5ad395b9784fe07780af0f5c214baa50b0ef5492
Pasindu Prabhashitha <it20610166@my.sliit.lk>
2022-04-26 21:47:58 +0530

    Add intents for navigation

--- app/src/main/java/com/mobileapplicationdevelopment/dogvio/Social_DogsList.java
+++ app/src/main/java/com/mobileapplicationdevelopment/dogvio/Social_DogsList.java
@@ -2,10 +2,21 @@ package com.mobileapplicationdevelopment.dogvio;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Social_DogsList extends AppCompatActivity {
+
+    CardView dogListCart;
+
+    private void onDogListCardClick(){
+        Intent intent = new Intent(this, Social_DogDetails.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +24,15 @@ public class Social_DogsList extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_social_dogs_list);
+
+
+        dogListCart = findViewById(R.id.socialDogCard);
+        dogListCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onDogListCardClick();
+            }
+        });
+
     }
 }
